e80b8b25a1f9446a6559dda3cba4fa3a41eebaf2
Daria Denisiuk <X00145914@outlook.com>
2020-12-18 00:43:30 +0000

    Unit Tests
    Added unit testing (not done)

---
+++ CA3UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+//using EADCA3
+//using EADCA3.Pages;
+
+
+namespace CA3UnitTest
+{
+    [TestClass]
+    public class UT1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // couldn't figure out the unit tests...
+        }
+    }
+}
